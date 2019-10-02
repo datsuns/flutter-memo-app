fd71ce3ff42d109bddc753d46384a6a144684252
datsuns <the.skeleton7@gmail.com>
2019-10-02 11:54:28 +0900

    change: apply todo-list's try result

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,209 @@
+// Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:core';
 
-void main() => runApp(MyApp());
+void main() => runApp(new TodoApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+        title: 'Todo List',
+        home: new TodoList()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class ToDoItem {
+  String _title;
+  String _body;
+  String _saveKey;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  ToDoItem(String text) {
+    this._body = text;
+    this._title = text.split('\n')[0];
+    this._saveKey  = DateTime.now().toString();
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  String title(){
+    return this._title;
+  }
 
-  final String title;
+  String body(){
+    return this._body;
+  }
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  String key(){
+    return this._saveKey;
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TodoList extends StatefulWidget {
+  @override
+  createState() => new TodoListState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class TodoListState extends State<TodoList> {
+  List<ToDoItem> _todoItems = [];
+  String latestInput = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedData();
+  }
+
+  void _loadSavedData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    for(var key in prefs.getKeys()){
+      _todoItems.add(new ToDoItem(prefs.getString(key)));
+    }
+  }
+
+  // Instead of auto generating a todo item, _addTodoItem now accepts a string
+  void _addTodoItem(String task) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // Putting our code inside "setState" tells the app that our state has changed,
+    // and it will automatically re-render the list
+    //             ^^^^^^^^^^^^^^^^^^^^^^^
+    // ==> calling setState() to invoke TodoList.createState(),
+    //     and that cause re-render
+    if( task.length > 0 ){
+      setState( () {
+        var item = new ToDoItem(task);
+        _todoItems.add(item);
+        prefs.setString(item.key(), item.body());
+      });
+    }
+  }
+
+  // Much like _addTodoItem, this modifies the array of todo strings and
+  // notifies the app that the state has changed by using setState
+  void _removeTodoItem(int index) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var target = _todoItems[index];
+    setState( () => _todoItems.removeAt(index));
+    await prefs.remove(target.title());
+  }
+
+
+  void _promptRemoveTodoItem(int index){
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text('Mark "${_todoItems[index]}" ad done?'),
+
+              actions: <Widget>[
+                new FlatButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()
+                ),
+
+                new FlatButton(
+                    child: new Text('DONE'),
+                    onPressed: (){
+                      _removeTodoItem(index);
+                      Navigator.of(context).pop();
+                    }
+                ),
+              ]
+          );
+        }
+    );
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      // itemBuilder will be automatically be called as many times as it takes
+      // for the list to fill up its available space, which is most likely
+      // more than the number of todo items we have.
+      // So, we need to check the index is OK.
+      // ignore: missing_return
+      itemBuilder: (context, index) {
+        if(index < _todoItems.length){
+          return _buildTodoItem(_todoItems[index].title(), index);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText, int index){
+    return new ListTile(
+        title: new Text(todoText),
+        onTap: () => _promptRemoveTodoItem(index)
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text('ToDo List')
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+
+      body: _buildTodoList(),
+
+      floatingActionButton: new FloatingActionButton(
+        // pressing this button now opens the new screen
+          onPressed: _pushAddTodoScreen,
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)
+      ),
+    );
+  }
+
+  Widget _generateTaskAddInput(BuildContext context) {
+    return new Scaffold(
+      body: new TextField(
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+
+        onChanged: (text) {
+          this.latestInput = text;
+        },
+
+        decoration: new InputDecoration(
+            hintText: 'Enter something todo ...',
+            contentPadding: const EdgeInsets.all(16.0)
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      floatingActionButton: new FloatingActionButton(
+          onPressed: (){
+            Navigator.pop(context); // Close the add todo screen
+            _addTodoItem(this.latestInput);
+          },
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)
+      ),
     );
   }
+
+  // MaterialPageRoute will automatically animate the screen entry,
+  // as well as adding a back button to close it
+  MaterialPageRoute _generateTaskAddView(BuildContext context) {
+    var page = new MaterialPageRoute(
+          builder: (context) {
+            return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text('Add new Task')
+              ),
+              body: _generateTaskAddInput(context),
+            );
+          }
+        );
+    return page;
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+      _generateTaskAddView(context)
+    );
+  }
+
 }
